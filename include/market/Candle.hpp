b0e497c7d9ccb2fabebd00cd5c84cf5c62e87595
Devashish Haldar <workfordevashishhaldar@gmail.com>
2026-07-09 15:55:33 +0530

    Add Candle domain model

---
+++ include/market/Candle.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <string>
+
+class Candle
+{
+private:
+    std::string timestamp_;
+    double open_;
+    double high_;
+    double low_;
+    double close_;
+    long long volume_;
+
+public:
+    Candle(const std::string& timestamp,double open,double high,
+    double low,double close,long long volume);
+
+    const std::string& getTimestamp() const;
+
+    double getOpen() const;
+    double getHigh() const;
+    double getLow() const;
+    double getClose() const;
+
+    long long getVolume() const;
+};
