3cb531b4f7e104a53846c717329dfff41a542f74
Devashish Haldar <workfordevashishhaldar@gmail.com>
2026-07-20 16:03:06 +0530

    Implement portfolio execution in backtest engine

--- include/engine/BacktestEngine.hpp
+++ include/engine/BacktestEngine.hpp
@@ -2,16 +2,18 @@
 
 #include "engine/MarketIterator.hpp"
 #include "strategy/Strategy.hpp"
-
+#include "portfolio/Portfolio.hpp"
 class BacktestEngine
 {
 private:
     MarketIterator iterator_;
     Strategy& strategy_;
+    Portfolio& portfolio_;
 
 public:
     BacktestEngine(const MarketData& marketData,
-                   Strategy& strategy);
+                   Strategy& strategy,
+                   Portfolio& portfolio);
 
     void run();
 };
